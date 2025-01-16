f6e27b4daf271d442e18f90cbe7330d521a27e0f
Sonu Kumar Singh <sksgkpvks@gmail.com>
2025-01-17 03:33:12 +0530

    [MCD] Label all the old machine sets to disable the scale up
    Co-authored-by: Shafeeque E S <shafeeque.e.s@sap.com>
    Co-authored-by: Ashish Ranjan Yadav  <ashish.ranjan.yadav@sap.com>

--- pkg/controller/deployment_inplace.go
+++ pkg/controller/deployment_inplace.go
@@ -12,6 +12,7 @@ import (
 	"github.com/gardener/machine-controller-manager/pkg/apis/machine/v1alpha1"
 	"github.com/gardener/machine-controller-manager/pkg/controller/autoscaler"
 	labelsutil "github.com/gardener/machine-controller-manager/pkg/util/labels"
+	"github.com/gardener/machine-controller-manager/pkg/util/provider/machineutils"
 
 	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -44,6 +45,15 @@ func (dc *controller) rolloutAutoInPlace(ctx context.Context, d *v1alpha1.Machin
 			Effect: "PreferNoSchedule",
 		},
 	)
+
+	if len(oldISs) > 0 && !dc.machineSetsScaledToZero(oldISs) {
+		// Label all the old machine sets to disable the scale up.
+		err := dc.labelMachineSets(ctx, oldISs, map[string]string{machineutils.LabelKeyMachineSetScaleUpDisabled: "true"})
+		if err != nil {
+			klog.Errorf("Failed to add label %s on all machine sets. Error: %s", machineutils.LabelKeyMachineSetScaleUpDisabled, err)
+			return err
+		}
+	}
 
 	if dc.autoscalerScaleDownAnnotationDuringRollout {
 		// Add the annotation on the all machinesets if there are any old-machinesets and not scaled-to-zero.
@@ -505,4 +515,23 @@ func (dc *controller) getMachinesForDrain(is *v1alpha1.MachineSet, readyForDrain
 		return machinesWithoutSelectedForUpdate[:readyForDrain], nil
 	}
 	return machinesWithoutSelectedForUpdate, nil
+}
+
+// labelMachineSets label all the machineSets with the given label
+func (dc *controller) labelMachineSets(ctx context.Context, MachineSets []*v1alpha1.MachineSet, labels map[string]string) error {
+	for _, machineSet := range MachineSets {
+
+		if machineSet == nil {
+			continue
+		}
+
+		labels := MergeStringMaps(machineSet.Labels, labels)
+		addLabelPatch := fmt.Sprintf(`{"metadata":{"labels":{%s}}}`, labelsutil.GetFormatedLabels(labels))
+
+		if err := dc.machineSetControl.PatchMachineSet(ctx, machineSet.Namespace, machineSet.Name, []byte(addLabelPatch)); err != nil {
+			return fmt.Errorf("failed to label MachineSet %s: %v", machineSet.Name, err)
+		}
+	}
+
+	return nil
 }
