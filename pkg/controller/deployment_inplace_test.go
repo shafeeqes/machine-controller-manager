e8a152f0d533d7ff45e052b4be3ffd8921edc4f3
Sonu Kumar Singh <sksgkpvks@gmail.com>
2025-03-03 02:24:58 +0530

    Add test for in-place updates
    Co-authored-by: Shafeeque E S <shafeeque.e.s@sap.com>
    Co-authored-by: Ashish Ranjan Yadav  <ashish.ranjan.yadav@sap.com>

--- pkg/controller/deployment_inplace_test.go
+++ pkg/controller/deployment_inplace_test.go
@@ -3,3 +3,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 package controller
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("deployment_inplace", func() {})
